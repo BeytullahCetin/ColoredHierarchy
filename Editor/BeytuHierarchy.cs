fd165805544ee98686b6c21a8e4fe7fdbbc20acb
Beytullah Çetin <44231507+BeytullahCetin@users.noreply.github.com>
2023-11-30 15:37:25 +0300

    Initial Commit

---
+++ Editor/BeytuHierarchy.cs
@@ -0,0 +1,112 @@
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace BeytuHierarchy
+{
+    [InitializeOnLoad]
+    public class StyleHierarchy
+    {
+        static string[] dataArray;//Find ColorPalette GUID
+        static string path;//Get ColorPalette(ScriptableObject) path
+        static BeytuHierarchySettings hierarchySettings;
+
+        private static bool isInitialized = false;
+
+        static StyleHierarchy()
+        {
+            Initialize();
+        }
+
+        public static void Initialize()
+        {
+            dataArray = AssetDatabase.FindAssets("t:BeytuHierarchySettings");
+            if (dataArray.Length == 0)
+            {
+                Debug.Log("<color=red>BeytuHierarchy asset not found!</color>");
+                return;
+            }
+            //We have only one color palette, so we use dataArray[0] to get the path of the file
+            path = AssetDatabase.GUIDToAssetPath(dataArray[0]);
+            hierarchySettings = AssetDatabase.LoadAssetAtPath<BeytuHierarchySettings>(path);
+            if (isInitialized == true)
+                EditorApplication.hierarchyWindowItemOnGUI -= OnHierarchyWindow;
+            EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyWindow;
+            isInitialized = true;
+        }
+
+        private static void OnHierarchyWindow(int instanceID, Rect selectionRect)
+        {
+            if (hierarchySettings.isEnabled == false) return;
+
+            //To make sure there is no error on the first time the tool imported in project
+            if (dataArray.Length == 0) return;
+            UnityEngine.Object instance = EditorUtility.InstanceIDToObject(instanceID);
+
+            if (instance == null)
+                return;
+
+            DrawBackground(instance, selectionRect, hierarchySettings.DrawBackground);
+            DrawEnableDisableToggle(instance, selectionRect, hierarchySettings.drawActivationToggle);
+        }
+
+        private static void DrawBackground(Object instance, Rect selectionRect, bool canDraw)
+        {
+            if (canDraw == false)
+                return;
+
+            string[] gameObjectName = instance.name.Split(" ");
+            string keyChar = gameObjectName[0];
+
+            ColorDesign design = hierarchySettings.GetColorDesign(keyChar);
+
+            if (design == null)
+                return;
+
+            //Remove the symbol(keyChar) from the name.
+            string newName = gameObjectName[1];
+
+            //Draw a rectangle as a background, and set the color.
+            EditorGUI.DrawRect(selectionRect, design.backgroundColor);
+
+            //Create a new GUIStyle to match the desing in colorDesigns list.
+            GUIStyle newStyle = new GUIStyle
+            {
+                alignment = design.textAlignment,
+                fontStyle = design.fontStyle,
+                normal = new GUIStyleState()
+                {
+                    textColor = design.textColor,
+                }
+            };
+
+            //Draw a label to show the name in upper letters and newStyle.
+            if ((instance as GameObject).activeSelf == true)
+                EditorGUI.LabelField(selectionRect, newName, newStyle);
+            else
+                EditorGUI.LabelField(selectionRect, "(DISABLED) " + newName, newStyle);
+        }
+
+        private static void DrawEnableDisableToggle(Object instance, Rect selectionRect, bool canDraw)
+        {
+            if (canDraw == false)
+                return;
+
+            GameObject go = instance as GameObject;
+
+            var r = new Rect(selectionRect.xMax - 16, selectionRect.yMin, 16, 16);
+
+            var wasActive = go.activeSelf;
+            var isActive = GUI.Toggle(r, wasActive, "");
+            if (wasActive != isActive)
+            {
+                go.SetActive(isActive);
+                if (EditorApplication.isPlaying == false)
+                {
+                    UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(go.scene);
+                    EditorUtility.SetDirty(go);
+                }
+            }
+        }
+    }
+}
---
+++ Editor/BeytuHierarchySettings.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace BeytuHierarchy
+{
+	public class BeytuHierarchySettings : ScriptableObject
+	{
+		public bool isEnabled = true;
+		public bool DrawBackground = true;
+		public bool drawActivationToggle = true;
+
+		public List<ColorDesign> colorDesigns = new List<ColorDesign>();
+
+		// private void OnValidate()
+		// {
+		// 	Debug.Log("SO OnValidate Çalıştı");
+		// 	StyleHierarchy.Initialize();
+		// }
+
+		public ColorDesign GetColorDesign(string keyChar)
+		{
+			foreach (ColorDesign colorDesign in colorDesigns)
+			{
+				if (colorDesign.keyChar.Equals(keyChar))
+					return colorDesign;
+			}
+			return null;
+		}
+
+		[MenuItem("Hierarchy/Setup BeytuHierarchy")]
+		public static void Setup()
+		{
+			BeytuHierarchySettings asset = ScriptableObject.CreateInstance<BeytuHierarchySettings>();
+			string pathPrefix = "Assets/BeytuHierarchy";
+			string path = "/BeytuHierarchySetting.asset";
+
+			ResetAndLoadInitialSettings(asset);
+			AssetDatabase.CreateAsset(asset, pathPrefix + path);
+			StyleHierarchy.Initialize();
+		}
+
+		public static void ResetAndLoadInitialSettings(BeytuHierarchySettings asset)
+		{
+			asset.colorDesigns.Clear();
+			asset.colorDesigns.Add(new ColorDesign("//", new Color32(0, 125, 255, 255), new Color32(255, 255, 255, 255), TextAnchor.MiddleCenter, FontStyle.Bold));
+			asset.colorDesigns.Add(new ColorDesign("///", new Color32(0, 140, 70, 255), new Color32(255, 255, 255, 255), TextAnchor.MiddleCenter, FontStyle.Bold));
+			asset.colorDesigns.Add(new ColorDesign("@", new Color32(255, 100, 0, 255), new Color32(255, 255, 255, 255), TextAnchor.MiddleCenter, FontStyle.Bold));
+		}
+	}
+
+	[System.Serializable]
+	public class ColorDesign
+	{
+		[Tooltip("Rename gameObject begin with this keychar")]
+		public string keyChar;
+		[Tooltip("Don't forget to change alpha to 255")]
+		public Color backgroundColor;
+		[Tooltip("Don't forget to change alpha to 255")]
+		public Color textColor;
+		public TextAnchor textAlignment;
+		public FontStyle fontStyle;
+
+		public ColorDesign(string keyChar, Color backgroundColor, Color textColor, TextAnchor textAlignment, FontStyle fontStyle)
+		{
+			this.keyChar = keyChar;
+			this.backgroundColor = backgroundColor;
+			this.textColor = textColor;
+			this.textAlignment = textAlignment;
+			this.fontStyle = fontStyle;
+		}
+	}
+}
